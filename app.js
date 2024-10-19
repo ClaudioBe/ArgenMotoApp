723be814e3d7f8fb2bdeb1c089b5cab1a0e0a5dd
matias-es <119085960+matias-es@users.noreply.github.com>
2024-10-19 13:30:46 -0300

    Agregado registro de motos

--- app.js
+++ app.js
@@ -12,6 +12,24 @@ const registrarCliente=()=>{
     localStorage.setItem("clientes",JSON.stringify(clientes))
 }
 
+const registrarMoto=()=>{
+    const motos=JSON.parse(localStorage.getItem("motos"));
+    const moto={
+        descripcion:document.getElementById("descripcion").value,
+        marca:document.getElementById("marca").value,
+        modelo:document.getElementById("modelo").value,
+        anio:document.getElementById("anio").value,
+        kilometraje:document.getElementById("kilometraje").value,
+        nro_chasis:document.getElementById("nro_chasis").value,
+        stock_actual:document.getElementById("stock_actual").value,
+        stock_minimo:document.getElementById("stock_minimo").value,
+        stock_maximo:document.getElementById("stock_maximo").value,
+        precio:document.getElementById("precio").value 
+    }
+    motos.push(moto)
+    localStorage.setItem("motos",JSON.stringify(motos))
+}
+
 const listarClientes=()=>{
     const clientes=JSON.parse(localStorage.getItem("clientes"));
 }
